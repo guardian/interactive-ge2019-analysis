34b3682e0586fce40b62a2c357015bbfbca49ee6
Josh Holder <jholder112233@gmail.com>
2019-11-05 10:45:26 +0000

    New data structure

--- atoms/default/client/js/app.js
+++ atoms/default/client/js/app.js
@@ -1,11 +1,11 @@
 import React,{render} from "react"
-import Slope from "shared/js/slope.js"
+import DemographicSlope from "shared/js/demographicSlope.js"
 
 const loadAndDraw = async() => {
     const dataRequest = await fetch("<%= path %>/data.json")
     const data = await dataRequest.json()
 
-    render(<Slope data={data}/>, document.querySelector(".interactive-wrapper"));
+    render(<DemographicSlope data={data}/>, document.querySelector(".interactive-wrapper"));
 }
 
 loadAndDraw();
