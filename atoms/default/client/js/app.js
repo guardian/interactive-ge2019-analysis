c0042765967a42b9d92200e27285a0573b084508
Antonio Voce <antonio.voce@guardian.co.uk>
2019-11-05 09:58:59 +0000

    add map WIP

--- atoms/default/client/js/app.js
+++ atoms/default/client/js/app.js
@@ -1,7 +1,9 @@
-import React,{render} from "react"
+import React,{ render } from "react"
 import Slope from "../../../../shared/js/slope.js"
+import Map from "../../../../shared/js/map.js"
 // import * as util from "../../../../shared/js/util.js"
 // console.log(util)
 // console.log(Slope, util)
 
+render(<Map />, document.querySelector(".gv-map"));
 render(<Slope />, document.querySelector(".interactive-wrapper"));
---
+++ shared/js/map.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+
+class Map extends Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      width: 1260
+    }
+  }
+
+  render() {
+
+    // const { results } = this.props
+
+    return <div class="test">map here</div>
+
+  }
+
+  componentDidMount() {
+
+    // const width = window.innerWidth
+
+    // this.setState({ width })
+
+  }
+}
+
+export default Map
