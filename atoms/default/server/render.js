49cfc7a4044eba3fe68bfd630cf31e31c1bf3fff
Josh Holder <jholder112233@gmail.com>
2019-11-26 10:23:52 +0000

    styles

--- atoms/default/server/render.js
+++ atoms/default/server/render.js
@@ -1,6 +1,3 @@
 export async function render() {
-    return `<div id='maps' class="interactive-wrapper interactive-wrapper--showcase"></div>
-    <div id='slope-demo' class="interactive-wrapper"></div>
-    <div id='slope-const' class="interactive-wrapper"></div>
-    <div id='scatter' class="interactive-wrapper"></div>`
+    return ``
 }
